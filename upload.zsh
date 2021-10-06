649896ae63d0c88ff6700af940a75db07c7649d1
Jorgen <jorgen@buildnode.io>
2021-10-05 17:29:47 -0700

    upload network

--- upload.zsh
+++ upload.zsh
@@ -1,9 +1,10 @@
 #!/bin/zsh
 
 threshold=${1:-100000}
+network=${2:-local}
 
 echo "Emptying buffer..."
-dfx canister --network ic call metarank emptyAssetBuffer
+dfx canister --network $network call metarank emptyAssetBuffer
 
 for file in ./art/*; do
     assetIndex=$(echo $file | sed -E "s/(\.\/art\/)([0-9]+)\.(webp)/\2/");\
@@ -12,11 +13,11 @@ for file in ./art/*; do
     echo "Uploading asset #$assetIndex, size: $byteSize"
     while [ $i -le $byteSize ]; do
         echo "chunk #$(($i/$threshold+1))..."
-        dfx canister --network ic call metarank uploadAssetBuffer "( vec {\
+        dfx canister --network $network call metarank uploadAssetBuffer "( vec {\
             vec { $(for byte in ${(j:;:)$(od -An -v -tuC $file)[@]:$i:$threshold}; echo "$byte;") };\
         })"
         i=$(($i+$threshold))
     done
     echo "Finalizing asset $assetIndex..."
-    dfx canister --network ic call metarank writeAssetBuffer "($assetIndex : nat, \"image/webp\")"
+    dfx canister --network $network call metarank writeAssetBuffer "($assetIndex : nat, \"image/webp\")"
 done
